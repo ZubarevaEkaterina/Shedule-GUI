216a4103007ee64a4097a8c3b486b4a3eed8c88b
zhil <eireen.zhyilyaeva.97@gmail.com>
2017-04-25 11:33:24 +0500

    k

--- Shedule/Teacher_form.cs
+++ Shedule/Teacher_form.cs
@@ -31,8 +31,7 @@ namespace Schedule
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            Main_form back = new Main_form();
-            back.ShowDialog();
+           
         }
 
         private void button2_Click(object sender, EventArgs e)
